e9967bcb70ab0945e20adfde99cdc1f033cb92bf
Sajeeb Khan <sbhossain567@gmail.com>
2025-07-14 00:10:44 +0600

    fix-3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,7 +50,7 @@
             @endif
         </header>
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
-            <h1>Hello World!</h1>
+            <h1>Hello World! oo</h1>
         </div>
 
         @if (Route::has('login'))
