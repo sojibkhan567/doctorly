f4e615c72ad8cfde1b6d3c38b20fd31f55c8e5f9
Sajeeb Khan <sbhossain567@gmail.com>
2025-07-15 07:20:36 +0600

    fix bug-2

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [DepartmentController::class, 'index']);
 Route::get('/create', [DepartmentController::class, 'create']);
 Route::post('/store', [DepartmentController::class, 'store']);
-Route::post('/admin/category/create', function() {
+Route::get('/admin/category/create', function() {
     return view('category');
 });
-Route::post('/admin/category/{id}', function(){
+Route::get('/admin/category/{id}', function(){
     return view('edit-category');
 });
